3d48226d44c06c0df149188a442e6747767d0e95
shofiecilia29 <shofie.cilia.hermawan-2022@vokasi.unair.ac.id>
2023-07-18 14:11:08 +0700

    modell

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,35 @@
 
 namespace App\Providers;
 
+use App\Models\Tag;
+use App\Models\Category;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Paginator::useBootstrap();
+        View::composer('*', function($view) {
+            $view->with('menu_categories', Category::with('children')->whereNull('category_id')->get());
+            $view->with('menu_tags', Tag::get());
+        }); 
     }
 }
